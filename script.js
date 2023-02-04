1d7f6ac6c4d387b9df271b9ddbec318408c36b60
Davidson Simões <davidson.simoes@gmail.com>
2023-02-04 15:09:13 -0300

    alterando css do menu

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function abrirmeno(){
-let BarraMenuAberto = document.getElementById ('barra-menu')
-if (BarraMenuAberto.style.width=="0px")
-{BarraMenuAberto.style.width="180px";
-
-}else{
-    BarraMenuAberto.style.width="0px"
-}
-}
+function abrirmeno() {
+    let DivMenu = document.getElementById ('barra-menu');
+    if (DivMenu.style.width == '' || DivMenu.style.width == '0px') {
+        document.getElementById('barra-menu').style.width="180px";
+    } else {
+        document.getElementById('barra-menu').style.width="0px"
+    }
+} 
 
